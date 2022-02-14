38dc2f210996f16b32a1fbb69538aba7dc18c089
Joashfeh <63983498+bebonger@users.noreply.github.com>
2022-02-14 10:57:37 +0800

    asd

--- Dalgo's Descent/Assets/Scripts/Enemy/EnemyIdleState.cs
+++ Dalgo's Descent/Assets/Scripts/Enemy/EnemyIdleState.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class EnemyIdleState : EnemyBaseState
 {
+    float time;
     public override void OnSLStatePostEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        time = 0f;
     }
 
     public override void OnSLStateNoTransitionUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        time += Time.deltaTime;
+        Debug.Log(time);
+
+        if (time > 3f)
+        {
+            animator.SetBool("IsPatrolling", true);
+        }
+
         if (Input.GetKey(KeyCode.P))
         {
             animator.SetBool("IsAttack", true);
