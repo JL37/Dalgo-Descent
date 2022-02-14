ff27ac06fca68f3b27867b45a9aea4edcf3528f9
jeryl lah <56944877+OpticalLuck@users.noreply.github.com>
2022-02-15 04:10:36 +0800

    Jumping Is done

--- Dalgo's Descent/Assets/Scripts/Input/PlayerController.cs
+++ Dalgo's Descent/Assets/Scripts/Input/PlayerController.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
+    [Header("Modifer")]
     [Range(0, 1)][SerializeField] private float runSpeed;
-    [Range(0, 1)] [SerializeField] private float walkSpeed;
+    [Range(0, 1)][SerializeField] private float slowSpeed;
+    [Range(0, 1)][SerializeField] private float walkSpeed;
     [Range(0, 1)][SerializeField] private float turnSpeed;
+    [SerializeField] private float jumpForce;
 
+    [Header("Variables")]
+    [SerializeField] private LayerMask GroundLayer;
     public Rigidbody PlayerRigidBody;
     public Animator PlayerAnimator;
     public PlayerInput InputScript;
     public Vector3 MoveDirection { get; private set; }
     public Quaternion Rotation { get; private set; }
+    public float Velocity { get; private set; }
     public bool IsMoving { get; private set; }
+    public bool IsRunning { get; private set; }
+    public bool IsJump { get; private set; }
+    public bool IsLanding { get; private set; }
+    public bool IsGrounded { get; private set; }
 
     private int IsWalkingHash;
+    private int JumpTriggerHash;
+    private int IsLandingHash;
+    private int IsGroundedHash;
+    private int VelocityHash;
     void Awake()
     {
         InputScript = GetComponent<PlayerInput>();
@@ -26,11 +40,27 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         IsWalkingHash = Animator.StringToHash("IsWalking");
+        JumpTriggerHash = Animator.StringToHash("JumpTrigger");
+        IsLandingHash = Animator.StringToHash("IsLanding");
+        IsGroundedHash = Animator.StringToHash("IsGrounded");
+        VelocityHash = Animator.StringToHash("Velocity");
     }
 
     void Update()
     {
-        PlayerAnimator.SetBool(IsWalkingHash, IsMoving);
+        IsGrounded = Physics.CheckSphere(gameObject.transform.position, 0.2f, GroundLayer) && !IsJump;
+        //Set to Landing - Going Down + Not Grounded
+        if (PlayerRigidBody.velocity.y < -0.0001f && !IsGrounded) 
+        {
+            IsJump = false;
+            IsLanding = true;
+        }
+        //Set to Grounded
+        else if (IsGrounded)
+        {
+            IsLanding = false;
+            IsGrounded = true;
+        }
 
         if (IsMoving)
         {
@@ -43,27 +73,67 @@ public class PlayerController : MonoBehaviour
             //Apply Movement Direction based on Camera Direction
             Rotation = Rotation * Quaternion.LookRotation(MoveDirection);
         }
+        else
+        {
+            Velocity = Mathf.Lerp(Velocity, 0, slowSpeed);
+        }
+
+        PlayerAnimator.SetBool(IsWalkingHash, IsMoving);
+        PlayerAnimator.SetFloat(VelocityHash, Velocity);
+        PlayerAnimator.SetBool(IsLandingHash, IsLanding);
+        PlayerAnimator.SetBool(IsGroundedHash, IsGrounded);
     }
 
     void OnAnimatorMove()
     {
+        float animationDelta = PlayerAnimator.deltaPosition.magnitude;
 
-        PlayerRigidBody.MovePosition(PlayerRigidBody.position + Rotation * Vector3.forward * PlayerAnimator.deltaPosition.magnitude);
+        if(IsGrounded)
+            PlayerRigidBody.MovePosition(PlayerRigidBody.position + Rotation * Vector3.forward * animationDelta);
+        else
+            PlayerRigidBody.MovePosition(PlayerRigidBody.position + Rotation * Vector3.forward * Velocity * Time.fixedDeltaTime);
+
         PlayerRigidBody.MoveRotation(Quaternion.Slerp(PlayerRigidBody.rotation, Rotation, turnSpeed));
     }
 
     #region InputAction
     public void OnMovement(InputAction.CallbackContext context)
     {
-        Debug.Log(context.action);
         var contextDirection = context.ReadValue<Vector2>();
         IsMoving = !context.canceled;
         MoveDirection = IsMoving ? walkSpeed * new Vector3(contextDirection.x, 0, contextDirection.y) : Vector3.zero;
+
+        if (IsMoving && !IsRunning)
+        {
+            Velocity = walkSpeed;
+        }
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        if (IsMoving)
+        {
+            IsRunning = !context.canceled;
+            Velocity = !context.canceled ? runSpeed : walkSpeed;
+        }
     }
 
     public void Fire(InputAction.CallbackContext context)
     {
         //Debug.Log(context.control + " " + context.phase);
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started && !IsJump && IsGrounded)
+        {
+            PlayerAnimator.SetTrigger(JumpTriggerHash);
+            PlayerRigidBody.AddForce(Rotation * Vector3.forward * jumpForce * Velocity, ForceMode.Impulse);
+            PlayerRigidBody.AddRelativeForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+
+            IsJump = true;
+            IsGrounded = false;
+        }
     }
     #endregion
 
