d46cf68a78709ad9a0620b8dc693dec1b509ec56
Joashfeh <63983498+bebonger@users.noreply.github.com>
2022-02-16 07:41:21 +0800

    fixed a few problems, enhanced the ai

--- Dalgo's Descent/Assets/Scripts/Enemy/FieldOfView.cs
+++ Dalgo's Descent/Assets/Scripts/Enemy/FieldOfView.cs
@@ -7,6 +7,7 @@ using UnityEngine.Animations.Rigging;
 public class FieldOfView : MonoBehaviour
 {
 
+    public float m_viewRadius;
     public float m_radius;
     [Range(0,360)]
     public float m_angle;
@@ -34,12 +35,15 @@ public class FieldOfView : MonoBehaviour
         while (true)
         {
             yield return wait;
-            m_canSeeTarget = CanSeeTarget(transform, m_playerRef.transform, m_angle, m_radius);
+            m_canSeeTarget = CanSeeTarget(transform, m_playerRef.transform, m_angle, m_viewRadius);
         }
     }
 
     private bool CanSeeTarget(Transform enemy, Transform target, float viewAngle, float viewRange)
     {
+        if (Vector3.SqrMagnitude(target.position - enemy.transform.position) <= 25f)
+            return true;
+
         Vector3 toTarget = target.position - enemy.transform.position;
         if (Vector3.Angle(enemy.transform.forward, toTarget) <= viewAngle)
         {
