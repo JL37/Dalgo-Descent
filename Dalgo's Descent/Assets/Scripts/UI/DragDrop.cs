447083a08a3dd694d20d2cfde307eb12638a62c4
Sean Lee <69341830+Mirai-UwU@users.noreply.github.com>
2022-02-17 01:34:22 +0800

    added drop and drop

---
+++ Dalgo's Descent/Assets/Scripts/UI/DragDrop.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler,IBeginDragHandler,IEndDragHandler,IDragHandler
+{
+    [SerializeField] private Canvas canvas;
+
+    private RectTransform m_RectTransform;
+    private CanvasGroup m_canvasGroup;
+    private void Awake()
+    {
+        m_RectTransform = GetComponent<RectTransform>();
+        m_canvasGroup = GetComponent<CanvasGroup>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+        m_canvasGroup.alpha = .6f;
+        m_canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        m_RectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor; //this object will follow the mouse cursor
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        m_canvasGroup.alpha = 1.0f;
+        m_canvasGroup.blocksRaycasts = true;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("On pointer down");
+    }
+}
