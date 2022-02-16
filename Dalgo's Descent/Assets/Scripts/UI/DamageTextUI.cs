cd929736652f8ccd32e18900ee7b866df9ce0ec2
Elmo Einstein <jevonliau@gmail.com>
2022-02-17 01:37:26 +0800

    Object pooling manager created

---
+++ Dalgo's Descent/Assets/Scripts/UI/DamageTextUI.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DamageTextUI : MonoBehaviour
+{
+    [Header("Variables")]
+    [SerializeField] float m_MaxFontSize = 48;
+    [SerializeField] float m_MaxHeight = 100;
+    [SerializeField] float m_MaxHorizontalOffSet = 60;
+
+    protected TMP_Text m_TextCmpt;
+
+    protected float m_CurrAngle = 0;
+    protected float m_MaxAngle = 180f;
+    protected float m_AngSpd = 200f;
+
+    protected bool m_ShowText = true;
+
+    protected Transform m_TransformToTrack;
+    protected Canvas m_Canvas;
+
+    protected float m_HeightOffSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_TextCmpt = GetComponent<TMP_Text>();
+        m_TextCmpt.fontSize = 0;
+        m_Canvas = GameObject.FindGameObjectWithTag("HUD").GetComponent<Canvas>();
+
+        if (m_ShowText)
+            gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (GameStateManager.Get_Instance.CurrentGameState == GameState.Paused) //Return when paused
+            return;
+
+        if (m_ShowText)
+            UpdateScale(m_MaxFontSize, 0.3f);
+        else
+            UpdateScale(0, 0.3f);
+
+        UpdateTransform(Time.fixedDeltaTime);
+    }
+
+    protected void UpdateTransform(float dt)
+    {
+        //Update positioning of UI above text
+        //Final position of marker above chest in world space
+        float offset = m_TransformToTrack.position.y + m_HeightOffSet;
+        Vector3 offsetPos = new Vector3(m_TransformToTrack.position.x, offset, m_TransformToTrack.position.z);
+
+        //Calculate "screen" position (NOT a canvas/recttransform position)
+        Vector2 canvasPos;
+        Vector2 screenPoint = Camera.main.WorldToScreenPoint(offsetPos);
+
+        //Convert screen pos to canvas/recttransform space (LEAVE CAMERA NULL IF SCREEN SPACE OVERLAY)
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(m_Canvas.GetComponent<RectTransform>(), screenPoint, null, out canvasPos);
+
+        m_TextCmpt.transform.localPosition = canvasPos;
+    }
+
+    protected void UpdateScale(float target, float lerp)
+    {
+        m_TextCmpt.fontSize = Mathf.Lerp(m_TextCmpt.fontSize, target, lerp);
+    }
+
+    public void Initialise(Transform transform, string str, float offset = 0)
+    {
+        m_TransformToTrack = transform;
+        GetComponent<TMP_Text>().text = str;
+
+        m_HeightOffSet = offset;
+        GetComponent<TMP_Text>().fontSize = 0;
+
+        gameObject.SetActive(true);
+    }
+}
