e0c85832721ce2597725ae857611cfe16d1b5efd
Elmo Einstein <jevonliau@gmail.com>
2022-02-17 10:53:29 +0800

    Damage text done liao

--- Dalgo's Descent/Assets/Scripts/UI/DamageTextUI.cs
+++ Dalgo's Descent/Assets/Scripts/UI/DamageTextUI.cs
@@ -10,12 +10,15 @@ public class DamageTextUI : MonoBehaviour
     [SerializeField] float m_MaxHeight = 100;
     [SerializeField] float m_MaxHorizontalOffSet = 60;
 
+    [Header("Adjustable things for the animation lmao")]
+    [SerializeField] float m_MaxXOffset = 90;
+    [SerializeField] float m_MaxYOffset = 150;
+    [Range(0, 1)]
+    [SerializeField] float m_AbsXSetFadeOutTrigger = 0.7f; 
+
     protected TMP_Text m_TextCmpt;
 
-    protected float m_CurrAngle = 0;
-    protected float m_MaxAngle = 180f;
-    protected float m_AngSpd = 200f;
-
+    protected float m_XOffSet = 0;
     protected bool m_ShowText = true;
 
     protected Transform m_TransformToTrack;
@@ -45,10 +48,38 @@ public class DamageTextUI : MonoBehaviour
         else
             UpdateScale(0, 0.3f);
 
-        UpdateTransform(Time.fixedDeltaTime);
+        StickToTarget();
+
+        QuadEquation(Time.fixedDeltaTime);
+        UpdateShowText();
     }
 
-    protected void UpdateTransform(float dt)
+    protected void UpdateShowText()
+    {
+        float absX = m_XOffSet / m_MaxXOffset;
+        if (absX >= m_AbsXSetFadeOutTrigger && m_ShowText)
+            m_ShowText = false;
+
+        //print(m_TextCmpt.fontSize + " Wow");
+        if (m_TextCmpt.fontSize <= 0 && !m_ShowText)
+            gameObject.SetActive(false);
+    }
+
+    protected void QuadEquation(float dt)
+    {
+        Vector2 canvasPos = m_TextCmpt.transform.localPosition;
+
+        canvasPos.x += m_XOffSet;
+        canvasPos.y += ReturnAbsY(m_XOffSet / m_MaxXOffset) * m_MaxYOffset;
+
+        m_TextCmpt.transform.localPosition = canvasPos;
+
+        //Add to x offset
+        float spd = 225 * dt;
+        m_XOffSet = m_XOffSet + spd > m_MaxXOffset ? m_MaxXOffset : m_XOffSet + spd;
+    }
+
+    protected void StickToTarget()
     {
         //Update positioning of UI above text
         //Final position of marker above chest in world space
@@ -67,17 +98,32 @@ public class DamageTextUI : MonoBehaviour
 
     protected void UpdateScale(float target, float lerp)
     {
+        if (m_TextCmpt.fontSize <= target + 1)
+        {
+            m_TextCmpt.fontSize = target;
+            return;
+        }
+
         m_TextCmpt.fontSize = Mathf.Lerp(m_TextCmpt.fontSize, target, lerp);
     }
 
     public void Initialise(Transform transform, string str, float offset = 0)
     {
+        //Set important stuff here
         m_TransformToTrack = transform;
         GetComponent<TMP_Text>().text = str;
-
         m_HeightOffSet = offset;
+
+        //Reset values
+        m_ShowText = true;
+        m_XOffSet = 0;
         GetComponent<TMP_Text>().fontSize = 0;
 
         gameObject.SetActive(true);
+    }
+
+    protected float ReturnAbsY(float xoffset) //Offset must be 0 to 1
+    {
+        return -4 * xoffset * xoffset + 4 * xoffset;
     }
 }
