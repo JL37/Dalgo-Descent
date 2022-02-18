6a196f029d1bb788d937c61d14a9a6e0009a8319
Sean Lee <69341830+Mirai-UwU@users.noreply.github.com>
2022-02-19 00:26:31 +0800

    updated ui

--- Dalgo's Descent/Assets/Scripts/MainGame/GameManager.cs
+++ Dalgo's Descent/Assets/Scripts/MainGame/GameManager.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public HealthBar healthBar;
+    public Health_EXP status;
     public PlayerStats playerStats;
     void Start()
     {
         playerStats = new PlayerStats();
-        healthBar.Setup(playerStats);
+        status.Setup(playerStats);
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -31,12 +31,23 @@ public class GameManager : MonoBehaviour
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
         }
-        if (Input.GetKeyDown(KeyCode.M))
+        if (Input.GetKeyDown(KeyCode.M)) // testing for receiving damage
         {
             Debug.Log("Attack");
-            playerStats.Damage(5);
+            playerStats.Received_Damage(5);
             print("health now is : " + playerStats.Health);
         }
-
+        if (Input.GetKeyDown(KeyCode.N)) //testing for exp
+        {
+            Debug.Log("EXP");
+            playerStats.EXP_Update(5);
+            print("EXP now is : " + playerStats.EXP);
+        }
+        if (Input.GetKeyDown(KeyCode.L)) //testing too add health
+        {
+            Debug.Log("Heal");
+            playerStats.Replenish_Health(5);
+            print("health now is  : " + playerStats.Health);
+        }
     }
 }
