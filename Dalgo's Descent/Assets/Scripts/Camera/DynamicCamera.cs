d43a0c1fe169acfabd332f8de0dca962b2d0ca27
Elmo Einstein <jevonliau@gmail.com>
2022-02-18 17:52:09 +0800

    Fixed pause menu bug

---
+++ Dalgo's Descent/Assets/Scripts/Camera/DynamicCamera.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class DynamicCamera : MonoBehaviour
+{
+    [Header("Adjustable variables")]
+    [SerializeField] float m_NonCombatDistance = 5f;
+    [SerializeField] float m_CombatDistance = 10f;
+    [SerializeField] float m_LerpSpd = 0.25f;
+
+    protected CinemachineFreeLook m_Camera;
+
+    protected float currDistance = 5f;
+    protected float scaleTop = 1f;
+    protected float botTop = 1f;
+
+    private void Awake()
+    {
+        scaleTop = 4f / 5f;
+        botTop = 1.3f / 5f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Camera = GetComponent<CinemachineFreeLook>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Math
+    }
+}
--- Dalgo's Descent/Assets/Scripts/MainGame/GameManager.cs
+++ Dalgo's Descent/Assets/Scripts/MainGame/GameManager.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    protected PauseController m_PauseController;
 
     void Start()
     {
-
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        m_PauseController = GetComponent<PauseController>();
     }
 
     void Awake()
