04b53f086debf7f3e52759c5c8adc9ad13bdb210
Joashfeh <63983498+bebonger@users.noreply.github.com>
2022-02-19 19:51:59 +0800

    d

--- Dalgo's Descent/Assets/Scripts/MainGame/GameManager.cs
+++ Dalgo's Descent/Assets/Scripts/MainGame/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using Cinemachine;
 
-public class GameManager : Singleton<GameManager>
+public class GameManager : Singleton<GameManager>   
 {
     [SerializeField] DynamicCamera m_Camera;
 
