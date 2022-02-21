97a6ccfab9c70e0bc83ee830e48901d3fa27870e
Elmo Einstein <jevonliau@gmail.com>
2022-02-21 18:20:52 +0800

    bru moment

---
+++ Dalgo's Descent/Assets/Scripts/Props/CoinParticle.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinParticle : MonoBehaviour
+{
+    private void OnParticleSystemStopped()
+    {
+        
+    }
+}
