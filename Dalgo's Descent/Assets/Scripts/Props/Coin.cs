51cf061ae76746f55c23c54920ffdf2e03d8583f
Elmo Einstein <jevonliau@gmail.com>
2022-02-21 18:06:21 +0800

    still doing coins particle system

--- Dalgo's Descent/Assets/Scripts/Props/Coin.cs
+++ Dalgo's Descent/Assets/Scripts/Props/Coin.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Linked objects")]
+    [SerializeField] Transform m_ModelTransform;
+
+    [Header("Variables to adjust")]
+    [SerializeField] float m_RotationSpd = 280f;
+    [SerializeField] float m_Duration = 0.75f;
+    [SerializeField] float m_MaxHeight = 0.3f;
+
+
+    //variables to reset la if not what
+    protected float m_CurrRadians = 0;
+
+    private void Update()
     {
-        //GetComponent<MeshRenderer>().material.color = Color.black;
+        UpdateRotate();
+        UpdatePosition();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdatePosition()
     {
-        
+        float spd = (Time.deltaTime / m_Duration) * Mathf.PI;
+
+        m_CurrRadians += spd;
+        if (m_CurrRadians > 2 * Mathf.PI)
+            m_CurrRadians -= 2 * Mathf.PI;
+
+        float newY = Mathf.Sin(m_CurrRadians) * m_MaxHeight;
+        m_ModelTransform.localPosition = new Vector3(0, newY, 0);
+    }
+
+    public void UpdateRotate()
+    {
+        //Animation
+        float rotSpd = m_RotationSpd * Time.deltaTime;
+        m_ModelTransform.Rotate(new Vector3(1, 0, 0) * rotSpd);
     }
 
     private void OnTriggerEnter(Collider other)
