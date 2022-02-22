0af946ad87f5e4581a3090570257e12243c093c1
Joashfeh <63983498+bebonger@users.noreply.github.com>
2022-02-22 16:21:29 +0800

    d

--- Dalgo's Descent/Assets/Scripts/Player/PlayerAnimationEvents.cs
+++ Dalgo's Descent/Assets/Scripts/Player/PlayerAnimationEvents.cs
@@ -15,23 +15,23 @@ public class PlayerAnimationEvents : MonoBehaviour
 
     public void PlayerAttack()
     {
-        foreach (Collider c in weapon.collisionEvents)
-        {
-            if (c == null) continue;
+        //foreach (Collider c in weapon.collisionEvents)
+        //{
+        //    if (c == null) continue;
 
-            if (c.gameObject.tag == "AI")
-            {
-                AI ai = c.gameObject.GetComponent<AI>();
-                if (ai.aiType == AI.AI_TYPE.AI_TYPE_ENEMY)
-                {
-                    Debug.Log("Hit");
-                    ((AIUnit)ai).EnemyHit(GetComponent<PlayerStats>().BasicAtk);
-                }
-                else if (ai.aiType == AI.AI_TYPE.AI_TYPE_BOSS)
-                {
-                    ((BossAI)ai).Damage(GetComponent<PlayerStats>().BasicAtk);
-                }
-            }
-        }
+        //    if (c.gameObject.tag == "AI")
+        //    {
+        //        AI ai = c.gameObject.GetComponent<AI>();
+        //        if (ai.aiType == AI.AI_TYPE.AI_TYPE_ENEMY)
+        //        {
+        //            Debug.Log("Hit");
+        //            ((AIUnit)ai).EnemyHit(GetComponent<PlayerStats>().BasicAtk);
+        //        }
+        //        else if (ai.aiType == AI.AI_TYPE.AI_TYPE_BOSS)
+        //        {
+        //            ((BossAI)ai).Damage(GetComponent<PlayerStats>().BasicAtk);
+        //        }
+        //    }
+        //}
     }
 }
