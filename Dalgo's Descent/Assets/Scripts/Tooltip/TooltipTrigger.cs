028f2cf2843781ad41337f6a9880a9f72f9fae08
Mirai-UwU <weichuanseanlee@gmail.com>
2022-02-23 13:03:25 +0800

    update some tooltip and setup on main

--- Dalgo's Descent/Assets/Scripts/Tooltip/TooltipTrigger.cs
+++ Dalgo's Descent/Assets/Scripts/Tooltip/TooltipTrigger.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
     public string details;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Tooltip_Warning.ShowTooltip_Static("You cannot use this skill yet!");
+    }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
-        //Tooltip.ShowTooltip_Static(details) ;
-        Tooltip_Warning.ShowTooltip_Static(details);
+        Tooltip.ShowTooltip_Static(details) ;
+        //Tooltip_Warning.ShowTooltip_Static(details);
     }
 
     public void OnPointerExit(PointerEventData eventData)
