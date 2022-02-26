7581eb9cd76dd268b63ea02da39f0d00d66d4cb8
Sean Lee <69341830+Mirai-UwU@users.noreply.github.com>
2022-02-26 23:04:30 +0800

    qweqweqw

--- Dalgo's Descent/Assets/Scripts/Skills/SkillSlot.cs
+++ Dalgo's Descent/Assets/Scripts/Skills/SkillSlot.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
+using System;
 
-public class SkillSlot : TooltipTrigger, IDropHandler
+public class SkillSlot : TooltipTrigger, IDropHandler, IPointerDownHandler
 {
     // public DragDrop dragdrop;
     public SkillObject AnchoredSkill;
     public string keycode;
     public Sprite baseImage;
+    private bool startListen = false;
+
+    public Image SkillBorderImage_1;
+    public TMP_Text SkillBorderTMP_1;
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log("OnDrop");
         // Debug.Log(dragdrop.getCurrentRect());
 
         if(eventData.pointerDrag != null) //if you are dragging a game object
-        {   
+        {
+            if(AnchoredSkill != null)
+            AnchoredSkill.RebindKey("");
             AnchoredSkill = eventData.pointerDrag.GetComponent<Skill>().SkillScriptable;
 
             // Rebind Skill
@@ -25,9 +33,7 @@ public class SkillSlot : TooltipTrigger, IDropHandler
             AnchoredSkill.RebindKey(keyCodeStore);
             Debug.Log("Rebinded \"" + AnchoredSkill.SkillName + "\" to " + keycode);
 
-            // Update tooltip info
-            header = AnchoredSkill.SkillName + " [" + keycode + "]";
-            body = AnchoredSkill.SkillDescription;
+            UpdateToolTipInfo();
 
             // update skillslot image
             GetComponent<Image>().sprite = eventData.pointerDrag.GetComponent<Image>().sprite;
@@ -36,15 +42,74 @@ public class SkillSlot : TooltipTrigger, IDropHandler
         }
     }
 
+    public void RebindSkill1()
+    {
+        //skill1_text.text = "Press a button";
+        SkillBorderImage_1.color = new Color(0.6f, 0.6f, 0.6f);
+        startListen = true;
+    }
+
     public void ResetSkill()
     {
         GetComponent<Image>().sprite = baseImage;
+        AnchoredSkill.RebindKey("");
         AnchoredSkill = null;
     }
 
     void Update()
     {
         TriggerActive = (AnchoredSkill != null);
+        
+    }
+
+    private void UpdateToolTipInfo()
+    {
+        // Update tooltip info
+        header = AnchoredSkill.SkillName + " [" + keycode + "]";
+        body = AnchoredSkill.SkillDescription;
+        SkillBorderImage_1.color = new Color(1.0f, 1.0f, 1.0f);
+    }
+
+    public void OnGUI() //keybind listener
+    {
+        if (this.startListen)
+        {
+            Event events = Event.current;
+            if (events.isKey && events.keyCode.ToString() != "LeftAlt")
+            {
+                string key = events.keyCode.ToString();
+                SkillBorderTMP_1.text = key;
+                //skill1_text.text = key;
+                keycode = key;
+                AnchoredSkill.RebindKey("<Keyboard>/" + keycode);
+                UpdateToolTipInfo();
+                this.startListen = false;
+            }
+            else if (events.isMouse)
+            {
+                string mouse = events.button.ToString();
+                switch (mouse)
+                {
+                    case "0":
+                        AnchoredSkill.RebindKey("<Mouse>/leftButton");
+                        keycode = "LMB";
+                        break;
+                    case "1":
+                        AnchoredSkill.RebindKey("<Mouse>/rightButton");
+                        keycode = "RMB";
+                        break;
+                    default:
+                        AnchoredSkill.RebindKey("<Mouse>/leftButton");
+                        keycode = "LMB";
+                        break;
+
+                }
+                AnchoredSkill.RebindKey(mouse);
+                UpdateToolTipInfo();
+                this.startListen = false;
+            }
+        }
+
     }
 
 }
